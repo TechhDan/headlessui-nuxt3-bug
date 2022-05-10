fbeb1e41085d521720fff1aebe2106c193c75eea
techhdan <tech.dan2689@gmail.com>
2022-05-09 20:59:02 -0400

    Example of Headless bug

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
